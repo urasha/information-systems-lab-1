7c966fbf00c7965c4944916084ea01b050373c49
Vladislav <413022@niuitmo.ru>
2025-09-28 17:50:28 +0300

    ci: fixing secrets

--- backend/src/test/java/ru/urasha/studygroup/StudygroupApplicationTests.java
+++ backend/src/test/java/ru/urasha/studygroup/StudygroupApplicationTests.java
@@ -2,8 +2,10 @@ package ru.urasha.studygroup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudygroupApplicationTests {
 
 	@Test
