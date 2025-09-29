32984219613ce0bd2210c31b584f7f1a1c765f72
Vladislav <413022@niuitmo.ru>
2025-09-29 13:24:43 +0300

    fix(helios): updated configs for helios

--- backend/src/main/java/ru/urasha/studygroup/config/CorsConfig.java
+++ backend/src/main/java/ru/urasha/studygroup/config/CorsConfig.java
@@ -4,23 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:5174",
-                        "http://localhost:5174/~s413022",
-                        "http://localhost:5173/~s413022",
-                        "http://localhost:3000",
-                        "https://se.ifmo.ru",
-                        "https://helios.cs.ifmo.ru",
-                        "http://helios.cs.ifmo.ru"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
